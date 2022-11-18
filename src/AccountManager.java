0a732ebeb095b4648f6e04eecfcb7c766f84cb8e
dkrivoshapova <d.krivoshapova@g.nsu.ru >
2022-11-18 17:57:57 +0700

    File Acount Manager

--- src/AccountManager.java
+++ src/AccountManager.java
@@ -3,5 +3,5 @@ import java.io.IOException;
 public interface AccountManager {
     void register(Account account) throws IOException;
     Account login(String email, String password) throws AccountBlockedException, WrongCredentialsException;
-    void removeAccount(String email, String password);
+    void removeAccount(String email, String password) throws WrongCredentialsException;
 }
